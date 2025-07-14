39efa6fa322c706ef81914028734f9f55e093c42
alejandro-zapeta <alejandro-zapeta@users.noreply.github.com>
2025-07-13 21:12:42 -0600

    fix

---
+++ app/PageClient.tsx
@@ -0,0 +1,30 @@
+// app/PageClient.tsx
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentUser, selectCurrentUser } from '@/slices/usersSlice';
+import Dashboard from '@/components/Dashboard';
+
+export default function PageClient() {
+  const params = useSearchParams();
+  const userParam = params.get('user');
+  const dispatch = useDispatch();
+  const user = useSelector(selectCurrentUser);
+
+  useEffect(() => {
+    if (userParam) dispatch(setCurrentUser(userParam));
+  }, [userParam, dispatch]);
+
+  if (!user) {
+    return (
+      <div className="container p-6">
+        <h1 className="title">No user</h1>
+        <p>Add ?user=casey to the URL.</p>
+      </div>
+    );
+  }
+
+  return <Dashboard />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCurrentUser, selectCurrentUser } from '@/slices/usersSlice';
-import Dashboard from '@/components/Dashboard';
+// app/page.tsx (Server component)
+import { Suspense } from 'react';
+import PageClient from './PageClient';
 
 export default function Page() {
-  const params = useSearchParams();
-  const userParam = params.get('user');
-  const dispatch = useDispatch();
-  const user = useSelector(selectCurrentUser);
-
-  useEffect(() => {
-    if (userParam) dispatch(setCurrentUser(userParam));
-  }, [userParam, dispatch]);
-
-  if (!user) {
-    return (
-      <div className="container p-6">
-        <h1 className="title">No user</h1>
-        <p>Add ?user=john to the URL.</p>
-      </div>
-    );
-  }
-
-  return <Dashboard />;
+  return (
+    <Suspense fallback={<div className="p-6">Loading...</div>}>
+      <PageClient />
+    </Suspense>
+  );
 }
